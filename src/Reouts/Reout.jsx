3d1d2dce926fbcd7144b2f68f74ee47da965fbdc
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-15 20:11:34 +0600

    React-commit

---
+++ src/Reouts/Reout.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const MyReactReouter = createBrowserRouter([
+    {
+        path:'/',
+        element: 
+    }
+])
+ 
+
+export default MyReactReouter;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import MyReactReouter from './Reouts/Reout'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
- 
+ <RouterProvider router={MyReactReouter}></RouterProvider>
   </React.StrictMode>,
 )
