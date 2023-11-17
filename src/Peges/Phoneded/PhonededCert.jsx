58c98a10f6fae2fed19f87bef122927b8578d82b
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-17 22:53:33 +0600

    img-commit

---
+++ src/Peges/Phoneded/PhonededCert.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const PhonededCert = ({ phone }) => {
+    const { brand_name, id, image, phone_name
+        , price, rating } = phone || {}
+
+    return (
+        <div>
+            <div className="relative flex max-w-[46rem] flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                <div className="relative w-2/5 m-0 overflow-hidden text-gray-700 bg-white rounded-r-none shrink-0 rounded-xl bg-clip-border">
+                    <img src={image} alt="" />
+
+                </div>
+                <div className="p-6">
+                    <h6 className="block mb-4 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-pink-500 uppercase">
+                        {brand_name}
+                    </h6>
+                    <h4 className="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                        {phone_name}
+                    </h4>
+                    <div className="flex justify-between ">
+                        <h2>Price: {price}</h2>
+                        <h1 className="ml-24">Rating:{rating}</h1>
+                    </div>
+
+                    <div className="mt-24">
+                        <a className="inline-block" href="#">
+                            <button
+                                className="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-pink-500 uppercase align-middle transition-all rounded-lg select-none hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                type="button">ADD TO FAVORITE<svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="2"
+                                    stroke="currentColor"
+                                    aria-hidden="true"
+                                    class="w-4 h-4">
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                                    ></path>
+                                </svg>
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PhonededCert;
