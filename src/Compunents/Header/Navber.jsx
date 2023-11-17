914b0de4a864ccdc60097fe0501a952e8d865f2b
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-18 03:18:29 +0600

    Good-commit

--- src/Compunents/Header/Navber.jsx
+++ src/Compunents/Header/Navber.jsx
@@ -29,6 +29,15 @@ const Navber = () => {
     return (
         <div>
             <div className="navbar bg-base-100 justify-between   shadow-lg rounded-2xl">
+            <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        {ReouterLink}
+      </ul>
+    </div>
+  
                 <div className="navbar-start">
 
                     <a className=" btn-ghost text-2xl font-bold ml-8">PHONE SHOP</a>
--- src/Peges/Phone/PhoneCart.jsx
+++ src/Peges/Phone/PhoneCart.jsx
@@ -1,11 +1,12 @@
+import Rating from "react-rating";
 import { Link } from "react-router-dom";
 
 
-const PhoneCart = ({phone}) => {
+const PhoneCart = ({ phone }) => {
 
-    const {brand_name, id, image, phone_name
-  , price, rating} = phone 
-    
+    const { brand_name, id, image, phone_name
+        , price, rating } = phone
+
 
 
     return (
@@ -14,32 +15,39 @@ const PhoneCart = ({phone}) => {
                 <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white h-96 rounded-xl bg-clip-border">
                     <img
                         src={image}
-                        className="object-cover w-full h-full"/>
+                        className="object-cover w-full h-full" />
                 </div>
-                
+
                 <div className="p-6">
-                  <p className="block bg-green-500 font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+                    <p className="block bg-green-500 font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
                         {brand_name}</p>
                     <div className="flex items-center justify-between mb-2">
-                       
+
                         <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
-                        {phone_name}</p>
+                            {phone_name}</p>
                         <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
-                           {price}</p>
-                        </div>
+                            {price}</p>
+                    </div>
                 </div>
-                <div>
-                    <p className="ml-8 -mt-8">{rating}</p>
-                </div>
-                <div className="p-6 pt-0">
-                 <Link to={`/phonesess/${id}`}>
-                 <button className="block w-full select-none rounded-lg bg-zinc-300 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                        type="button">SEE DETAILES</button>
-                 </Link>
+           
+
+                <div className="rating">
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+</div>
+                   
+                    <div className="p-6 pt-0">
+                        <Link to={`/phonesess/${id}`}>
+                            <button className="block w-full select-none rounded-lg bg-zinc-300 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                type="button">SEE DETAILES</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+            );
 };
 
-export default PhoneCart;
+            export default PhoneCart;
