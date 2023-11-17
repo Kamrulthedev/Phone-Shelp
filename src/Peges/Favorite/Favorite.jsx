11f6abbc7c6c2b165b636b40f82b0db0df5e4858
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-18 01:10:04 +0600

    Found-commit

--- src/Peges/Favorite/Favorite.jsx
+++ src/Peges/Favorite/Favorite.jsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
 
 
 const Favorite = () => {
-    return (
+
+    const [favorited, setFavorited] = useState([])
+
+    const [Nofound, setNodFound] = useState(false)
+
+    useEffect(() => {
+
+        const FevoriteItem = JSON.parse(localStorage.getItem('Favorite'))
+
+        if (FevoriteItem) {
+            setFavorited(FevoriteItem)
+        }
+        else (
+          
+            setNodFound("No Data Found")
+
+        )
+    }, [])
+
+    console.log(favorited)
+
+
+    return( 
         <div>
-            <h1>MY FAVORITE WEBSITE</h1>
+              {
+                Nofound ? <p className="h-[80vh] flex ">{Nofound}</p>: <div></div>
+              }
         </div>
-    );
+    )
 };
 
 export default Favorite;
